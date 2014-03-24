4968c79e5d4053c3cb7d7a7f70af0af6034f0b91
Max Jacobson <max@hardscrabble.net>
2014-03-23 21:12:26 -0400

    sort by count on index

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,8 @@ class Link < ActiveRecord::Base
   before_create :generate_uid
   validate :is_a_url
 
+  default_scope -> { order(count: :desc) }
+
   def generate_uid
     self.uid = SecureRandom.hex(2) until !self.uid.nil? && Link.where(uid: self.uid).count.zero?
   end
