c6375f672702fec6dfa95158f874f6d26943b836
Max Jacobson <max@hardscrabble.net>
2014-03-23 20:18:28 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitly::Application.config.secret_key_base = '567c0f0f131b5b13458106a6cffd8b9afac99ed1777ff2ee56de183f63dec8f3db488adffba72711970fb64f4ea5201444d543b457d7c3b1382ddca785f5648e'
